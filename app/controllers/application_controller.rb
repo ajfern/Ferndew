88fd869d26bf96a90bb6e763f92e014312f19c2e
Adam Fern <adamf@squareup.com>
2012-03-06 21:28:02 -0800

    Initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,23 @@ class ApplicationController < ActionController::Base
   def index
     @todews = Todew.all
   end
+
+  def update
+    # render :text => params.inspect
+    # 1. Go over all of your todews
+    # 2. Look at if the completed state is different from what is inside of params
+    # 3. Change as necessary
+    Todew.all.each do |betterit|
+      if betterit.completed == true && !params[betterit.id.to_s].present?
+        #then betterit.completed = false #don't need then on a sep line
+        betterit.completed = false
+        betterit.save!
+      end
+      if betterit.completed == false && params[betterit.id.to_s]=='on' 
+        betterit.completed = true
+        betterit.save!
+      end
+    end
+    redirect_to '/'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -56,4 +56,6 @@ Ferndew::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
   match '/', :controller=>'application',:action=>'index'
+  match '/update', :controller=>'application',:action=>'update'
+  
 end
